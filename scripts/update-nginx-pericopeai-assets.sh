e1bc0a2ece3658ec3de8b3572193c54d77988799
benlagrone <benjaminlagrone@gmail.com>
2026-04-29 17:22:17 -0500

    Escape nginx vars in assets vhost helper

--- scripts/update-nginx-pericopeai-assets.sh
+++ scripts/update-nginx-pericopeai-assets.sh
@@ -21,10 +21,10 @@ server {
 
     location / {
         proxy_pass http://pericopeai_assets;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }
 CONF
@@ -37,7 +37,7 @@ upstream pericopeai_assets { server ${UPSTREAM}; }
 server {
     listen 80;
     server_name ${DOMAIN};
-    return 301 https://$host$request_uri;
+    return 301 https://\$host\$request_uri;
 }
 
 server {
@@ -51,10 +51,10 @@ server {
 
     location / {
         proxy_pass http://pericopeai_assets;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }
 CONF
